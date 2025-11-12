94c182265eb85d5cf1070e75c8947affb7f9da7c
Maykel Santos <55636141+maykelsantoz@users.noreply.github.com>
2025-11-11 23:10:14 -0300

    feat: add page components

---
+++ app/_components/header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { MenuIcon, MessageCircleIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+// import SidebarMenu from "./sidebar-menu";
+import { Button } from "./ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between bg-white px-5 py-6">
+      <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
+      <div className="flex items-center gap-2">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/chat">
+            <MessageCircleIcon />
+          </Link>
+        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[370px] p-0">
+            <SheetHeader className="border-b px-5 py-6 text-left">
+              <SheetTitle className="text-lg font-bold">Menu</SheetTitle>
+            </SheetHeader>
+            {/* <SidebarMenu /> */}
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
